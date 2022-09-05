09c2ce4bf3e15fa260c806af644a3cde16b8f2e1
Oliver <okennedy@buffalo.edu>
2022-09-05 13:27:54 -0400

    Adding timing output

--- src/main/scala/cse250/examples/Week1RecitationExample.scala
+++ src/main/scala/cse250/examples/Week1RecitationExample.scala
@@ -10,14 +10,20 @@ object Week1RecitationExamples
       Source.fromFile("Solar_Electric_Programs_truncated.csv")
     
     var coordinates = Seq[(Double, Double)]()
+    var lastTime = System.currentTimeMillis
 
     for( (row, index) <- data.getLines().zipWithIndex )
     {
-      if(index % 1000 == 0){ println(s"Processing row $index") }
+      if(index % 1000 == 0){ 
+        val now = System.currentTimeMillis
+        println(s"Processing row $index (${now - lastTime}ms since last check)") 
+        lastTime = now
+      }
       val columns = row.split(",")
       coordinates = coordinates :+ ( (columns(columns.length-3).toDouble, columns(columns.length-2).toDouble) )
     }
     
-    /* do something with coordinates */
+    /* the code would now 
+    do something with coordinates */
   }
 }
